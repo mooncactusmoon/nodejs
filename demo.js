bcd920d29978c3677a0a39268edabc17b5e84918
Moon <mooncactusmoon@gmail.com>
2022-03-17 15:18:49 +0800

    初步了解 exports 和 module.exports

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
-// console.log('HELLO World');
-// console.log(this);
-var fs =require('fs');
-var data =fs.readFileSync('abc.txt');
-console.log(data);
-console.log('toString'+data.toString());
+// var colors=require('colors'); //先裝好colors套件
+// console.log('Moon'.magenta+' hi'.yellow);
+
+var m1= require('./module1');
+// m1.run(); //exports.run
+m1(); // module.exports 
---
+++ module1.js
@@ -0,0 +1,6 @@
+// exports.run = function(){ //expots為現有的物件，不可改變，只能增加屬性
+//    console.log("I am module1 :>");
+// }
+module.exports = function(){
+   console.log("I am module1 :>");
+}
