4c214dd6af354d8d45aa8dd47c6d5197ddbf2180
Moon <mooncactusmoon@gmail.com>
2022-03-17 16:18:28 +0800

    process理解

--- demo.js
+++ demo.js
@@ -1,6 +1,11 @@
 // var colors=require('colors'); //先裝好colors套件
 // console.log('Moon'.magenta+' hi'.yellow);
-console.log("demo.js => " + __filename);
-var m1= require('./module1');
-// m1.run(); //exports.run
-m1(); // module.exports 
+
+// console.log("demo.js => " + __filename);
+// var m1= require('./module1');
+// // m1.run(); //exports.run
+// m1(); // module.exports 
+
+// console.log(process.argv);
+// console.log(process.env.tmp);
+console.log(process.cwd());
