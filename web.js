f97396ae9d22660d9377a4e22a10c7eab99df645
Moon <mooncactusmoon@gmail.com>
2022-03-19 17:19:18 +0800

    用mime(npm套件)來查看檔案類型

--- web.js
+++ web.js
@@ -1,17 +1,18 @@
+var mime = require('mime'); //這個npm模組可以確認副檔名
 var url = require('url'); //可設定要得到的路徑內容
 var fs = require('fs'); //模組
 var http = require('http');
 var server = http.createServer(function(request,response){
     var pathname= url.parse(request.url).pathname; //網址不取到get參數
     // console.log(pathname);
-    response.writeHead(200,{
-        "Content-Type": "text/html"
-    })
     fs.stat('.' + pathname, function(err,stats){
         if(!err && stats.isFile()){
             //err空的且stats是個檔案
             fs.readFile("./index.html",function(err,html){
                 if(!err){ //err是空的，代表沒有錯誤
+                    response.writeHead(200,{
+                        "Content-Type": mime.getType(pathname)
+                    })
                     response.write(html); //可以寫很多個response.write
                     response.end(); //結尾一定要加上response.end
                 }
