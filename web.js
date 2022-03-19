25e65ccf2287f0220f18d1f5d2275db7b2ba83db
Moon <mooncactusmoon@gmail.com>
2022-03-19 16:37:18 +0800

    用fs的方式呈現index.html內容於3000port上

--- web.js
+++ web.js
@@ -1,6 +1,11 @@
+var fs = require('fs'); //模組
 var http = require('http');
 var server = http.createServer(function(request,response){
-    response.write('Hello World :>'); //可以寫很多個response.write
+    response.writeHead(200,{
+        "Content-Type": "text/html"
+    })
+    var html=fs.readFileSync("./index.html"); //同步讀取
+    response.write(html); //可以寫很多個response.write
     response.end(); //結尾一定要加上response.end
 });
 server.listen(3000,'127.0.0.1');
