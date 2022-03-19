533a3adcc361c7fe4fde45cb90459a6fe9871eca
Moon <mooncactusmoon@gmail.com>
2022-03-19 18:17:16 +0800

    用sha1+crypto來檢查快取

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var mime = require('mime'); //這個npm模組可以確認副檔名
 var url = require('url'); //可設定要得到的路徑內容
 var fs = require('fs'); //模組
 var http = require('http');
+var http = require('crypto'); //這個模組可以定義演算法
+
 var server = http.createServer(function(request,response){
     var pathname= url.parse(request.url).pathname; //網址不取到get參數
     // console.log(pathname);
@@ -21,8 +23,15 @@ var server = http.createServer(function(request,response){
             //err空的且stats是個檔案
             fs.readFile(relativePathname,function(err,html){
                 if(!err){ //err是空的，代表沒有錯誤
+                    var hash = crypto.createHash('sha1').update(html).digest('base64');
+                    if (request.headers['if-none-match'] == hash) {
+                        response.writeHead(304);
+                        response.end();
+                        return;
+                    } 
                     response.writeHead(200,{
-                        "Content-Type": mime.getType(pathname)
+                        "Content-Type": mime.getType(pathname),
+                        "Etag": hash
                     })
                     response.write(html); //可以寫很多個response.write
                     response.end(); //結尾一定要加上response.end
